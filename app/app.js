d289ee627b2d464a87617bb440ecfa9bb5dbfe94
Ricky <ricky.holtz@berkeley.edu>
2017-01-19 13:13:18 -0500

    Cleanup

--- app/app.js
+++ app/app.js
@@ -1,7 +1,18 @@
 module.exports = {
-  calculateFibonacci: function (n) {
-    if (n === 0 || n === 1) {
-      return 1
+  calculateFibonacci: function (n, sequence) {
+    let fibonacciSequence = sequence || []
+    if (n < 0) {
+      return false
     }
+    for (var i = 0; i < n + 1; i++) {
+      if (i < 2) {
+        fibonacciSequence.push(1)
+      } else {
+        const lastNumber = fibonacciSequence[i - 1]
+        const penultimateNumber = fibonacciSequence[i - 2]
+        fibonacciSequence.push(lastNumber + penultimateNumber)
+      }
+    }
+    return fibonacciSequence[n]
   }
 }
--- app/tests.js
+++ app/tests.js
@@ -2,10 +2,22 @@ const test = require('tape')
 const app = require('./app.js')
 
 test('base case', function (assert) {
-  assert.plan(2)
+  assert.plan(6)
 
   assert.equal(app.calculateFibonacci(0), 1, '0th place should return 1')
   assert.equal(app.calculateFibonacci(1), 1, '1st place should return 1')
+  assert.equal(app.calculateFibonacci(2), 2, '2nd place should return 2')
+  assert.equal(app.calculateFibonacci(3), 3, '3rd place should return 3')
+  assert.equal(app.calculateFibonacci(4), 5, '4th place should return 5')
+  assert.equal(app.calculateFibonacci(10), 89, '10th place should return 89')
+
+  assert.end()
+})
+
+test('edge case', function (assert) {
+  assert.plan(1)
+
+  assert.notOk(app.calculateFibonacci(-1), 'negative number isn\'t truthy')
 
   assert.end()
 })
