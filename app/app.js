a4db7b291cc1af98aa9ed4452447ff0334b39fab
Ricky <ricky.holtz@berkeley.edu>
2017-02-06 01:15:45 -0800

    Return multiple fibonacci numbers

--- app/app.js
+++ app/app.js
@@ -1,11 +1,12 @@
 module.exports = {
-  calculateFibonacci: function (n, sequence) {
+  calculateFibonacci: function (n) {
     'use strict'
-    let fibonacciSequence = sequence || []
+    let fibonacciSequence = []
+
     if (n < 0) {
       return false
     }
-    for (var i = 0; i < n + 1; i++) {
+    for (let i = 0; i < n + 1; i++) {
       if (i < 2) {
         fibonacciSequence.push(i)
       } else {
@@ -14,6 +15,48 @@ module.exports = {
         fibonacciSequence.push(lastNumber + penultimateNumber)
       }
     }
+
     return fibonacciSequence[n]
+  },
+
+  calculateFibonacciArray: function (n) {
+    'use strict'
+    let fibonacciSequence = []
+
+    if (n < 0) {
+      return false
+    }
+    for (let i = 0; i < n + 1; i++) {
+      if (i < 2) {
+        fibonacciSequence.push(i)
+      } else {
+        const lastNumber = fibonacciSequence[i - 1]
+        const penultimateNumber = fibonacciSequence[i - 2]
+        fibonacciSequence.push(lastNumber + penultimateNumber)
+      }
+    }
+
+    return fibonacciSequence
+  },
+
+  findMultipleFibonacci: function () {
+    const values = Object.keys(arguments).map((key) => {
+      return arguments[key]
+    })
+
+    var n = Math.max(...values)
+
+    const fibonacciSequence = this.calculateFibonacciArray(n)
+
+    let returnArray = []
+    for (let i = 0; i < arguments.length; i++) {
+      returnArray.push(fibonacciSequence[arguments[i]])
+    }
+
+    if (returnArray.length === 1) {
+      return returnArray[0]
+    } else {
+      return returnArray
+    }
   }
 }
--- app/tests.js
+++ app/tests.js
@@ -1,7 +1,7 @@
 const test = require('tape')
 const app = require('./app.js')
 
-test('base case', function (assert) {
+test('sequence is correct', function (assert) {
   assert.plan(6)
 
   assert.equal(app.calculateFibonacci(0), 0, '0th place should return 0')
@@ -18,6 +18,14 @@ test('edge case', function (assert) {
   assert.plan(1)
 
   assert.notOk(app.calculateFibonacci(-1), 'negative number isn\'t truthy')
+
+  assert.end()
+})
+
+test('can return n fibonacci numbers', function (assert) {
+  assert.plan(2)
+  assert.same(app.findMultipleFibonacci(0, 5), [0, 5], '0th and 5th places should return 0 and 5')
+  assert.same(app.findMultipleFibonacci(1, 10, 2), [1, 55, 1], '1st, 10th, and 2nd places should return 1, 55, and 1')
 
   assert.end()
 })
